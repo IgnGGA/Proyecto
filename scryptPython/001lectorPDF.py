92bd7e9dd50b1b0ef81c1a225ccfbce045a8108b
Ignacio Gamboa <ign.gga@outlook.cl>
2024-12-25 22:52:25 -0300

    entregas una lista con las mrutas de cada uno de los archivos

--- scryptPython/001lectorPDF.py
+++ scryptPython/001lectorPDF.py
@@ -15,24 +15,5 @@ def rutasOSTT(directorio):
             break
     return rutas
 
-rutasOSTT('OrdenesDeServicioTécnicoAndover')
-
-pdf = PDFQuery('OrdenesDeTrabajoTrilogy/202401/Orden de servicio técnico Andover.118924-287932.pdf')
-pdf.load()
-text_elements = pdf.pq('LTTextLineHorizontal')
-text = [t.text for t in text_elements]
-print(text)
-
-a,b=0,0
-dim=len(text)
-
-while True:
-    try:
-        for a in range (dim):
-            if text[-a]=='' or text[-a]==' ':
-                text.pop(-a)
-        break
-    except IndexError:
-        b=b+1
-        print(f'Fin iteración:\t{b}')
-print(text)
+rutasArchivos=rutasOSTT('OrdenesDeServicioTécnicoAndover/')
+print(rutasArchivos[3])
