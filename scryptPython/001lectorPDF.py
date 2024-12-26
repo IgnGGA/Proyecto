52a5316714f1a4b0beed4daf028d7a6b4f5b6664
Ignacio Gamboa <ign.gga@outlook.cl>
2024-12-25 23:50:13 -0300

    se genera script que toma las rutas, y obtiene los datos del archivo

--- scryptPython/001lectorPDF.py
+++ scryptPython/001lectorPDF.py
@@ -15,6 +15,17 @@ def rutasOSTT(directorio):
             break
     return rutas
 
+def LectorOSSTT(rutaArchivos):
+    dataOSSTT=[]
+    for i in range(len(rutaArchivos)):
+        pdf = PDFQuery(rutaArchivos[i])
+        pdf.load()
+        text_elements = pdf.pq('LTTextLineHorizontal')
+        text = [t.text for t in text_elements]
+        dataOSSTT.append(text)
+    return dataOSSTT
+
 rutasArchivos=rutasOSTT('OrdenesDeServicioTécnicoAndover/')
-print(rutasArchivos)
 print(f'Tipo de dato:\t{type(rutasArchivos)}\nTamaño del dato:\t{len(rutasArchivos)}')
+datos=LectorOSSTT(rutasArchivos)
+print(f'Tipo de dato:\t{type(datos)}\nTamaño del dato:\t{len(datos)}')
